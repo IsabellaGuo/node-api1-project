53780541d676c1543a85706d79b58c8c21507725
IsabellaGuo <isabella.guofei@gmail.com>
2020-02-10 22:16:26 -0800

    Finished Get and Delete

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const Users = require('./data/db');
 
 const server = express();
 
+const port = 5000;
+
+server.listen(port, () => console.log(`\n** API on port ${port} \n`));
+
 //teaches express how to read JSON from the body
 server.use(express.json()); //needed for POST, PUT and PATCH
 
@@ -12,23 +16,37 @@ server.get('/', (req, res) => {
     res.json({ hello: 'Isabella'})
 })
 
-//create a user 
+//POST - create a user 
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
 
+    if (!(userInfo && userInfo.name && userInfo.bio)){
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+        });
+        return true;
+    }
+
+    if (!(typeof userInfo.name === "string" && typeof userInfo.bio === "string")) {
+        res.status(400).json({
+            errorMessage: "Some fields have incorrect data"
+        });
+        return true;
+    }
+
     Users
-    .add(userInfo)
+    .insert(userInfo)
     .then(user => {
-        res.status(201).json(user);
+        res.status(201).json({ ...userInfo, ...user });
     })
     .catch(err => {
         console.log(err);
-        res.status(500).json({ errorMessage: 'oops'})
-    })
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database"});
+    });
     
-})
+});
 
-//view all the user objects
+//GET - view all the user objects
 server.get('/api/users', (req, res) => {
     Users
     .find()
@@ -38,11 +56,55 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => {
         console.log(err);
-        res.status(500).json({ errorMessage: 'oops'})
+        res.status(500).json({ errorMessage: "The users information could not be retrieved."})
+    });
+})
+
+//GET - Return user object with id
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    Users
+    .findById(id)
+    .then(users => {
+        if (users) {
+            res.status(200).json(users);
+        } else {
+            res.status(404).json({
+                errorMessage: "The user with the specified ID does not exist."
+            });
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({errorMessage: "The user information could not be retrieved."})
     });
 })
 
 
-const port = 5000;
 
-server.listen(port, () => console.log(`\n** API on port ${port} \n`));
+
+//DELETE - remove user with specific id
+server
+.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    Users
+    .remove(id)
+    .then(users =>{
+        if (users) {
+            res.status(200).json(users);
+        } else {
+            res.status(404).json({
+                errorMessage: "The user with the specified ID does not exist."
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({errorMessage: "The user could not be removed"})
+    });
+})
+
+//PUT - Update the user with specified id
